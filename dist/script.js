a68c038701425c2d2534cab07245d764b9c0d504
JoshTeflon <jenikele21h@gmail.com>
2020-11-18 16:59:19 +0100

    js changes

--- dist/script.js
+++ dist/script.js
@@ -1,8 +1,22 @@
+document.onreadystatechange = function() { 
+  if (document.readyState !== "complete") { 
+      document.querySelector( 
+        "body").style.visibility = "hidden"; 
+      document.querySelector( 
+        ".preloader").style.visibility = "visible"; 
+  } else { 
+      document.querySelector( 
+        ".preloader").style.display = "none"; 
+      document.querySelector( 
+        "body").style.visibility = "visible"; 
+  } 
+}; 
+
 const baseUrl = "https://api.github.com/graphql";
 
 myHeaders = {
   "Content-Type": "application/json",
-  Authorization: "bearer 970fc5e98e520f4010eaae8e5955b50ca784c381"
+  Authorization: "bearer 9369af86652f4938b57bfb7076382c9cee27302a"
 };
 
 const queryFetch = (query) => {
@@ -68,6 +82,7 @@ queryFetch(
     </li>
 </ul>`
 ).then(text => (document.getElementById('section-nav').innerHTML = text))
+ .then(text => (document.getElementById('section-nav-mobile').innerHTML = text))
  .catch(err => console.error(err, "error"));
 
 
@@ -99,13 +114,13 @@ queryFetch(
 queryFetch(
   `query {
     user(login: "JoshTeflon") {
-      repositories(last: 20, orderBy: {field:CREATED_AT, direction:DESC}) {
+      repositories(last: 1, orderBy: {field:CREATED_AT, direction:DESC}) {
         nodes {
           name
           description
           primaryLanguage {
-            color
             name
+            color
           }
           stargazerCount
           forkCount
@@ -114,42 +129,43 @@ queryFetch(
       }
     }
   }`
-).then(({data}) => {
-  data.user.repositories.nodes.map(repo => {
-    return `<div class="repo-search">
-    <input type="text" placeholder="Find a repository..." />
-  </div>
-  <div class="repo-details">
-    <div>
-      <h3 class="repo-name">${repo.name}</h3>
-      <div class="repo-description">${repo.description}</div>
-    <ul>
-      <li><span class="repo-language-color" style="background-color: #f1e05a"></span></li>
-      <li class="star-fork">
+).then(({data}) => 
+`<div class="repo-search">
+  <input type="text" placeholder="Find a repository..." />
+</div>
+<div class="repo-details">
+  <div>
+    <h3 class="repo-name">${data.user.repositories.nodes[0].name}</h3>
+    <div class="repo-description">${data.user.repositories.nodes[0].description}</div>
+  <ul>
+    <li>
+      <span class="repo-language-color" style="background-color: ${data.user.repositories.nodes[0].primaryLanguage.color}"></span>
+      ${data.user.repositories.nodes[0].primaryLanguage.name}
+    </li>
+    <li class="star-fork">
+      <svg viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
+        <path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"></path>
+      </svg>
+        ${data.user.repositories.nodes[0].stargazerCount}
+    </li>
+    <li class="star-fork">
+      <svg aria-label="fork" viewBox="0 0 16 16" version="1.1" width="16" height="16" role="img">
+        <path fill-rule="evenodd" d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"></path>
+      </svg>
+        ${data.user.repositories.nodes[0].forkCount}
+    </li>
+    <li>Updated on ${data.user.repositories.nodes[0].updatedAt}</li>
+  </ul>
+</div>
+  <div>
+    <button>
         <svg viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
-          <path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"></path>
+              <path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"></path>
         </svg>
-          ${repo.stargazerCount}
-      </li>
-      <li class="star-fork">
-        <svg aria-label="fork" viewBox="0 0 16 16" version="1.1" width="16" height="16" role="img">
-          <path fill-rule="evenodd" d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"></path>
-        </svg>
-          ${repo.forkCount}
-      </li>
-      <li>${repo.updatedAt}</li>
-    </ul>
-  </div>
-    <div>
-      <button>
-          <svg viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
-                <path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"></path>
-          </svg>
-          Star
-      </button>
-  </div>
-  </div>`
-  }).join('');
-}
-).then(text => (document.getElementById('section-right').innerHTML = text))
+        Star
+    </button>
+</div>
+</div>`
+)
+.then(text => (document.getElementById('section-right').innerHTML = text))
 .catch(err => console.log(err, "error"));
